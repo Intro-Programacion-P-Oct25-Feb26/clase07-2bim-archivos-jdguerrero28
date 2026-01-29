404954743de1b60763482f68df53b11d6914a43c
jdguerrero28 <jdiegogueal@gmail.com>
2026-01-29 08:56:21 -0500

    avance

--- ManejoArchivos/src/lectura05/Estadisticas.java
+++ ManejoArchivos/src/lectura05/Estadisticas.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -5,6 +6,8 @@
 package lectura05;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Estadisticas {
@@ -18,19 +21,15 @@ public class Estadisticas {
 
             while (entrada.hasNext()) {
                 String linea = entrada.nextLine();
+
                 try {
-                    if (linea == null || linea.trim().isEmpty()) {
-                        throw new Exception("Línea vacía");
+                    ArrayList<String> partes = 
+                            new ArrayList<>(Arrays.asList(linea.split("\\|")));
+
+                    if (partes.size() < 3) {
+                        throw new Exception("Faltan datos");
                     }
-
-                    String[] partes = linea.split("\\|");
-
-                    if (partes.length < 3) {
-                        throw new Exception("Faltan datos o "
-                                + "formato incorrecto");
-                    }
-
-                    int empleados = Integer.parseInt(partes[2]);
+                    int empleados = Integer.parseInt(partes.get(2));
 
                     sumaEmpleados = sumaEmpleados + empleados;
                     contador = contador + 1;
@@ -44,9 +43,8 @@ public class Estadisticas {
         } catch (Exception e) {
             System.err.println("Error general");
         }
-
         sumaEmpleados = sumaEmpleados / contador;
         System.out.printf("Promedio es: %.2f\n", sumaEmpleados);
-    }
 
+    }
 }
